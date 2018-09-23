3d940eb9b9a9c0e20b3ca302e5fa5160282acef3
Daniel Bälz <me@dbaelz.de>
2018-09-23 21:33:20 +0200

    Test error cases of the args parser

--- native/src/test/kotlin/ParserTest.kt
+++ native/src/test/kotlin/ParserTest.kt
@@ -1,8 +1,10 @@
-import kotlin.test.*
 import de.dbaelz.konclik.Command
 import de.dbaelz.konclik.Parameter
 import de.dbaelz.konclik.ParseResult
 import de.dbaelz.konclik.parseArgs
+import kotlin.test.Test
+import kotlin.test.assertEquals
+import kotlin.test.assertTrue
 
 open class ParserTest {
 
@@ -12,8 +14,8 @@ open class ParserTest {
 
         assertTrue(result is ParseResult.Parameters)
         if (result is ParseResult.Parameters) {
-            assertEquals(emptyMap<String, String>(), result.positionalArguments)
-            assertEquals(emptyMap<String, List<String>>(), result.options)
+            assertEquals(emptyMap(), result.positionalArguments)
+            assertEquals(emptyMap(), result.options)
         }
     }
 
@@ -57,6 +59,95 @@ open class ParserTest {
             assertEquals(listOf("value1Something"), result.options["--value1"])
             assertEquals(listOf("value2Something", "value2Default1"), result.options["--value2"])
             assertEquals(listOf("one"), result.options["--choice1"])
+        }
+    }
+
+
+    // Parsing: Error Cases
+
+    @Test
+    fun `Option not available`() {
+        val command = Command("test",
+                options = listOf(Parameter.Option.Value("--value1", 1, listOf("value1Default"))))
+        val args = listOf("--value1", "value1Something", "--value2", "value2Something")
+
+        val result = parseArgs(command, args)
+
+        assertTrue(result is ParseResult.Error)
+        if (result is ParseResult.Error) {
+            assertEquals(ParseResult.Error.Code.NO_OPTION_AVAILABLE, result.code)
+        }
+    }
+
+    @Test
+    fun `Not enough values for value option`() {
+        val command = Command("test",
+                options = listOf(Parameter.Option.Value("--value1", 2, emptyList())))
+        val args = listOf("--value1", "somevalue")
+
+        val result = parseArgs(command, args)
+
+        assertTrue(result is ParseResult.Error)
+        if (result is ParseResult.Error) {
+            assertEquals(ParseResult.Error.Code.NOT_ENOUGH_VALUES_FOR_OPTION, result.code)
+        }
+    }
+
+    @Test
+    fun `No value provided for choice option`() {
+        val command = Command("test",
+                options = listOf(Parameter.Option.Choice("--value1", setOf("one", "two"))))
+        val args = listOf("--value1")
+
+        val result = parseArgs(command, args)
+
+        assertTrue(result is ParseResult.Error)
+        if (result is ParseResult.Error) {
+            assertEquals(ParseResult.Error.Code.NOT_ENOUGH_VALUES_FOR_OPTION, result.code)
+        }
+    }
+
+    @Test
+    fun `Incorrect value provided for choice option`() {
+        val command = Command("test",
+                options = listOf(Parameter.Option.Choice("--value1", setOf("one", "two"))))
+        val args = listOf("--value1", "three")
+
+        val result = parseArgs(command, args)
+
+        assertTrue(result is ParseResult.Error)
+        if (result is ParseResult.Error) {
+            assertEquals(ParseResult.Error.Code.INCORRECT_CHOICE_VALUE_PROVIDED, result.code)
+        }
+    }
+
+    @Test
+    fun `More positional arguments provided than expected`() {
+        val command = Command("test",
+                arguments = listOf(Parameter.Argument("argument1")))
+        val args = listOf("somearg", "anotherarg")
+
+        val result = parseArgs(command, args)
+
+        assertTrue(result is ParseResult.Error)
+        if (result is ParseResult.Error) {
+            assertEquals(ParseResult.Error.Code.MORE_POSITIONAL_ARGUMENTS_THAN_EXPECTED, result.code)
+        }
+    }
+
+    @Test
+    fun `Positional argument after option`() {
+        val command = Command("test",
+                arguments = listOf(Parameter.Argument("argument1")),
+                options = listOf(Parameter.Option.Switch("--switch1"),
+                        Parameter.Option.Value("--value1", 1, listOf("value1Default"))))
+        val args = listOf("--switch1", "someargument", "--value1", "value1Something")
+
+        val result = parseArgs(command, args)
+
+        assertTrue(result is ParseResult.Error)
+        if (result is ParseResult.Error) {
+            assertEquals(ParseResult.Error.Code.POSITIONAL_ARGUMENT_AFTER_OPTION, result.code)
         }
     }
 }
